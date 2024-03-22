9043df4b22b158695c6e9b1cc89fb65883b9dc80
h0x32 <42139189+h0x32@users.noreply.github.com>
2024-03-22 02:38:01 +0200

    Create cachyos-proton-lutris.sh
    Added 1st script to execute 1st tip

---
+++ cachyos-proton-lutris.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script will setup proton from cachyos repo in a way that can be used in Lutris
+
+LUTRIS_WINE_DIR="/home/$USER/.local/share/lutris/runners/wine"
+PROTON_CACHYOS_INSTALL_DIR="/usr/share/steam/compatibilitytools.d"
+
+mkdir -p $LUTRIS_WINE_DIR/{proton-8-cachyos,proton-9-cachyos}
+
+ln -s $PROTON_CACHYOS_INSTALL_DIR/proton-cachyos/files/* $LUTRIS_WINE_DIR/proton-9-cachyos/
+ln -s $PROTON_CACHYOS_INSTALL_DIR/proton-8-cachyos/files/* $LUTRIS_WINE_DIR/proton-8-cachyos/
